c9acfe0eff78671ce38525ce77ac09f085bfa7ba
Julien Church <julienchurch@gmail.com>
2016-07-07 20:37:37 +0000

    Remove cruft

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,4 +3,7 @@
 ln -s $HOME/.vim/vimrc $HOME/.vimrc
 ln -s $HOME/.vim/gvimrc $HOME/.gvimrc
 
+git submodule add https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
+
 
