8f1421eb927b6c8122d145199d6287099b6ba0bc
Julien Church <julienchurch@gmail.com>
2016-08-01 13:45:20 -0700

    Remove gvimrc

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
 ln -s $HOME/.vim/vimrc $HOME/.vimrc
-ln -s $HOME/.vim/gvimrc $HOME/.gvimrc
-
 mkdir ./bundle ./undo
 git submodule add https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
